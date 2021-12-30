caeada5367e462a1c7006d1c584bfe9741e235e2
Emilia <emilia.delezaeta@keepler.io>
2021-12-30 15:24:08 +0100

    some updates

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# udacity-data-warehouse-aws
-Data Engineering Nanodegree - data warehouse project with S3 and redshift
+# Data Modeling with Postgres
+
+## Introduction
+
+A music streaming startup, Sparkify, has grown their user base and song database and want to move their processes and data onto the cloud. Their data resides in S3, in a directory of JSON logs on user activity on the app, as well as a directory with JSON metadata on the songs in their app.
+
+As their data engineer, you are tasked with building an ETL pipeline that extracts their data from S3, stages them in Redshift, and transforms data into a set of dimensional tables for their analytics team to continue finding insights in what songs their users are listening to. You'll be able to test your database and ETL pipeline by running queries given to you by the analytics team from Sparkify and compare your results with their expected results.
+
+
+## Data Model 
+
+This project use a single fact table to store measure data *(songplays)* and few dimension tables to store some attributes about data.
+
+![](udacity-sparkifydb-data-model.jpg)
+
+**songplays** --> records in log data associated with song plays i.e. records with page ```NextSong```.
+
+**users** --> users in the app.
+
+**songs** --> songs in music database.
+
+**artists** --> artists in music database.
+
+**time** --> timestamps of records in **songplays** broken down into specific units.
+
+
+## ETL Pipeline
+
+There are few steps to run in order to insert data into the tables.
+
+**sql_queries.py** : DDL to create the structure of the database and DML to insert and manipulate data in the database.
+
+**create_tables.py** : establishes the connection to the sparkify database in a Redshift cluster (AWS) and call the previous step. The cluster was created before as Iac with the notebook ```create_redshift_cluster.ipynb```
+
+**etl.py** : read, transform and load data into the new database using a staging area where the files stored in S3 were COPY into Redshift.
+
+
+## Example Queries
+
+Some queries were executed to test the connection and check if the database is available and running, few examples are in the notebook ```querying_redshift_cluster.ipynb```
+
+## Hints
+
+Doing the project some important point were found and I'like to share them:
+
+1. The best way to load a big amount of data into redshift is using the COPY command, for this project was used  **COPY from JSON format** and sometime you have to indicate the JSON structure of the file in the COPY. To see more documentation: [COPY from JSON format](https://docs.aws.amazon.com/redshift/latest/dg/copy-usage_notes-copy-from-json.html)
+
+2. Another good point about redshift COPY command is that it can be parametrized, for example TIMESTAMP columns can be formatted as ```TIMEFORMAT [AS] {'timeformat_string' | 'auto' | 'epochsecs' | 'epochmillisecs' }```. To see more documentation:
+ [Data conversion parameters](https://docs.aws.amazon.com/redshift/latest/dg/copy-parameters-data-conversion.html#copy-timeformat)
+
+3. To reduce the timming to read **song_data** I chose a group of file following the S3 link ```'s3://udacity-dend/song_data/A/A'```
+
+4. Before to create a new IAM role, you must create a new IAM user in your AWS account, give it **AdministratorAccess** from Attach existing policies directly tab and take note of the access key and secret
+
+5. To insert data into the final tables some columns were formatted using CAST function. To see documentation: [CAST and CONVERT functions](https://docs.aws.amazon.com/redshift/latest/dg/r_CAST_function.html)
--- sql_queries.py
+++ sql_queries.py
@@ -1,71 +1,210 @@
 import configparser
 
-
 # CONFIG
+
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
-staging_events_table_create= ("""
+'''the following 2 staging tables will be used used as landing zone and all data types are VARCHAR to avoid conflicts 
+copying data from S3, in the ETL the data types will be mapped correctly '''
+'''but the column TS will be mapped as TIMESTAMP because in the COPY to redshift it can be transformed to epochmillisecs'''
+'''documentation COPY --> https://docs.aws.amazon.com/redshift/latest/dg/copy-parameters-data-conversion.html#copy-timeformat '''
+
+staging_events_table_create = (""" CREATE TABLE IF NOT EXISTS staging_events (
+                                    artist VARCHAR,
+                                    auth VARCHAR,
+                                    firstName VARCHAR,
+                                    gender VARCHAR,
+                                    itemInSession VARCHAR,
+                                    lastName VARCHAR,
+                                    length VARCHAR,
+                                    level VARCHAR,
+                                    location VARCHAR,
+                                    method VARCHAR,
+                                    page VARCHAR,
+                                    registration VARCHAR,
+                                    sessionId VARCHAR,
+                                    song VARCHAR,
+                                    status VARCHAR,
+                                    ts TIMESTAMP,
+                                    userAgent VARCHAR,
+                                    userId VARCHAR
+                                );
+
 """)
 
-staging_songs_table_create = ("""
+staging_songs_table_create = (""" CREATE TABLE IF NOT EXISTS staging_songs (
+                                    num_songs VARCHAR,
+                                    artist_id VARCHAR,
+                                    artist_latitude VARCHAR,
+                                    artist_longitude VARCHAR,
+                                    artist_location VARCHAR(max),
+                                    artist_name VARCHAR(max),
+                                    song_id VARCHAR,
+                                    title VARCHAR,
+                                    duration VARCHAR,
+                                    year VARCHAR
+                            );
 """)
 
-songplay_table_create = ("""
+'''Maybe this fact table could be big, it's better to use a distkey to distribute data to each node and improve joins 
+on this table '''
+'''There is 2 options to chose a distkey, start_time or user_id that are coming from dimensions tables and NOT NULL'''
+'''In this case, fact table and dimension table must use the same distribution key'''
+
+songplay_table_create = (""" CREATE TABLE IF NOT EXISTS songplays (
+                               songplay_id INT IDENTITY(0,1) PRIMARY KEY, 
+                               start_time TIMESTAMP NOT NULL sortkey,
+                               user_id INT NOT NULL distkey,
+                               level VARCHAR,
+                               song_id VARCHAR,
+                               artist_id VARCHAR,
+                               session_id INT,
+                               location VARCHAR,
+                               user_agent VARCHAR
+                            );
 """)
 
-user_table_create = ("""
+user_table_create = (""" CREATE TABLE IF NOT EXISTS users (
+                                 user_id INT PRIMARY KEY distkey, 
+                                 first_name VARCHAR NOT NULL, 
+                                 last_name VARCHAR NOT NULL,
+                                 gender VARCHAR, 
+                                 level VARCHAR
+                             ); 
 """)
 
-song_table_create = ("""
+'''the following dimension tables will use distribution style ALL to replicate the table to each node to speed up 
+joins '''
+
+song_table_create = (""" CREATE TABLE IF NOT EXISTS songs(
+                                    song_id VARCHAR PRIMARY KEY sortkey, 
+                                    title VARCHAR NOT NULL, 
+                                    artist_id VARCHAR, 
+                                    year INT, 
+                                    duration DECIMAL 
+                                ) diststyle ALL;
 """)
 
-artist_table_create = ("""
+artist_table_create = (""" CREATE TABLE IF NOT EXISTS artists(
+                                artist_id VARCHAR PRIMARY KEY sortkey,
+                                name VARCHAR, 
+                                location VARCHAR, 
+                                latitude FLOAT, 
+                                longitude FLOAT
+                            ) diststyle ALL;
 """)
 
-time_table_create = ("""
+time_table_create = (""" CREATE TABLE IF NOT EXISTS time(
+                                start_time TIMESTAMP PRIMARY KEY sortkey, 
+                                hour INT, 
+                                day INT, 
+                                week INT, 
+                                month INT, 
+                                year INT, 
+                                weekday INT
+                            ) diststyle ALL;
 """)
 
 # STAGING TABLES
 
-staging_events_copy = ("""
-""").format()
+'''documentation COPY JSON --> https://docs.aws.amazon.com/redshift/latest/dg/copy-usage_notes-copy-from-json.html '''
 
-staging_songs_copy = ("""
-""").format()
+staging_events_copy = (""" COPY staging_events 
+                           FROM {}
+                           CREDENTIALS 'aws_iam_role={}'
+                           JSON {}
+                           TIMEFORMAT as 'epochmillisecs'
+                           REGION 'us-west-2';
+""").format(config['S3']['LOG_DATA'], config['IAM_ROLE']['ARN'], config['S3']['LOG_JSONPATH'])
+
+staging_songs_copy = (""" COPY staging_songs 
+                          FROM {}
+                          CREDENTIALS 'aws_iam_role={}'
+                          JSON 'auto'  
+                          REGION 'us-west-2';
+""").format(config['S3']['SONG_DATA'], config['IAM_ROLE']['ARN'])
 
 # FINAL TABLES
 
-songplay_table_insert = ("""
-""")
+''' Some columns are formatted (CAST) before insert them into the final table'''
+
+songplay_table_insert = (""" 
+          INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location,user_agent)
+            SELECT  ts,
+                    CAST(events.userId as integer),
+                    events.level,
+                    songs.song_id,
+                    songs.artist_id,
+                    CAST(events.sessionId as integer),
+                    events.location,
+                    events.userAgent
+            FROM staging_events events 
+            INNER JOIN staging_songs songs ON (events.artist = songs.artist_name AND events.song = songs.title)
+            WHERE events.page = 'NextSong' 
+    """)
 
 user_table_insert = ("""
+        INSERT INTO users (user_id, first_name, last_name, gender, level)
+            SELECT DISTINCT CAST(userId as integer),
+                    firstName,
+                    lastName, 
+                    gender, 
+                    level
+            FROM staging_events
+            WHERE page = 'NextSong'
 """)
 
 song_table_insert = ("""
+        INSERT INTO songs (song_id, title, artist_id, year, duration)
+            SELECT DISTINCT song_id,
+                title, 
+                artist_id, 
+                CAST(year as integer),
+                CAST(duration as decimal)
+            FROM staging_songs
 """)
 
-artist_table_insert = ("""
+artist_table_insert = (""" 
+        INSERT INTO artists (artist_id, name, location, latitude, longitude)
+            SELECT DISTINCT artist_id, 
+                artist_name,
+                artist_location,
+                CAST(artist_latitude as float), 
+                CAST(artist_longitude as float)
+            FROM staging_songs
 """)
 
 time_table_insert = ("""
+        INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+            SELECT ts, 
+                EXTRACT(hour from ts),
+                EXTRACT(day from ts),
+                EXTRACT(week from ts),
+                EXTRACT(month from ts),
+                EXTRACT(year from ts),
+                EXTRACT(weekday from ts)
+            FROM staging_events
 """)
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create,
+                        user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop,
+                      song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert,
+                        time_table_insert]
